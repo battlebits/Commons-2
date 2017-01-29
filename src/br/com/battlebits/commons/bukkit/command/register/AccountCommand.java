da63b359326fd01bb99649ac35b1995348bb95c7
Gustavo Inacio <flame.tuner@gmail.com>
2017-01-29 00:55:39 -0200

    Arrumado problema com comando /ip

--- src/br/com/battlebits/commons/bukkit/command/register/AccountCommand.java
+++ src/br/com/battlebits/commons/bukkit/command/register/AccountCommand.java
@@ -1,5 +1,19 @@
 package br.com.battlebits.commons.bukkit.command.register;
 
-public class AccountCommand {
+import br.com.battlebits.commons.bukkit.command.BukkitCommandArgs;
+import br.com.battlebits.commons.core.command.CommandClass;
+import br.com.battlebits.commons.core.command.CommandFramework.Command;
+
+public class AccountCommand implements CommandClass {
+
+	@Command(name = "account", aliases = { "acc", "conta" })
+	public void account(BukkitCommandArgs args) {
+
+	}
+
+	@Command(name = "preferences", aliases = { "prefs", "preferencias" })
+	public void preferences(BukkitCommandArgs args) {
+
+	}
 
 }
