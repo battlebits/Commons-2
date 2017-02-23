ec8e935ece574198245b29e9d249c7a24e0e60d4
Luãn Pereira <mrluan.gamer@outlook.com>
2017-02-23 15:13:36 -0300

    AntiAFK melhorado

--- src/br/com/battlebits/commons/bukkit/listener/AntiAFK.java
+++ src/br/com/battlebits/commons/bukkit/listener/AntiAFK.java
@@ -25,16 +25,14 @@ public class AntiAFK implements Listener {
 	private static Map<UUID, Integer> afkMap = new HashMap<>();
 
 	public void addTime(Player p) {
-		int time = 0;
 		if (!BukkitMain.getInstance().isAntiAfkEnabled())
 			return;
-		if (afkMap.containsKey(p.getUniqueId()))
-			time = afkMap.get(p.getUniqueId());
+		int time = afkMap.getOrDefault(p.getUniqueId(), 0);
 		afkMap.put(p.getUniqueId(), ++time);
 	}
 
-	public static int getTime(Player p) {
-		return afkMap.containsKey(p.getUniqueId()) ? afkMap.get(p.getUniqueId()) : 0;
+	public static int getTime(Player p) {		
+		return afkMap.getOrDefault(p.getUniqueId(), 0);
 	}
 
 	public void resetTimer(Player p) {
@@ -42,42 +40,49 @@ public class AntiAFK implements Listener {
 	}
 
 	@EventHandler(priority = EventPriority.LOWEST)
-	public void onPlayerQuitListener(UpdateEvent event) {
+	public void onUpdate(UpdateEvent event) {
 		if (event.getType() != UpdateType.SECOND)
 			return;
 		if (!BukkitMain.getInstance().isAntiAfkEnabled())
 			return;
 		for (Player p : Bukkit.getOnlinePlayers()) {
-			Location loc = locations.get(p.getUniqueId());
-			if (loc == null) {
-				locations.put(p.getUniqueId(), p.getLocation().clone());
-				continue;
-			}
-			Location l = p.getLocation();
-			if (loc.getX() == l.getX() && loc.getY() == l.getY() && loc.getZ() == l.getZ()) {
-				addTime(p);
-			} else {
-				resetTimer(p);
-			}
 			BattlePlayer player = BattlebitsAPI.getAccountCommon().getBattlePlayer(p.getUniqueId());
 			if (player == null)
 				return;
 			if (!player.hasGroupPermission(Group.LIGHT)) {
-				if (getTime(p) == 240) {
+				
+				Location loc = locations.get(p.getUniqueId());
+				if (loc == null) {
+					locations.put(p.getUniqueId(), p.getLocation().clone());
+					continue;
+				}
+				
+				Location l = p.getLocation();
+				if (loc.getX() == l.getX() && loc.getY() == l.getY() && loc.getZ() == l.getZ()) {
+					addTime(p);
+				} else {
+					resetTimer(p);
+				}
+				
+				int time = getTime(p);
+				
+				if (time == 240) {
 					p.sendMessage("§%antiafk-timer%§");
-					break;
+					continue;
 				}
-				if (getTime(p) >= 300) {
+				
+				if (time >= 300) {
 					p.kickPlayer(Translate.getTranslation(player.getLanguage(), "antiafk-kick"));
-					break;
+					continue;
 				}
+				
+				locations.put(p.getUniqueId(), p.getLocation().clone());
 			}
-			locations.put(p.getUniqueId(), p.getLocation().clone());
 		}
 	}
 
 	@EventHandler(priority = EventPriority.LOWEST)
-	public void onPlayerJoinListener(PlayerQuitEvent e) {
+	public void onLeave(PlayerQuitEvent e) {
 		locations.remove(e.getPlayer().getUniqueId());
 		afkMap.remove(e.getPlayer().getUniqueId());
 	}
