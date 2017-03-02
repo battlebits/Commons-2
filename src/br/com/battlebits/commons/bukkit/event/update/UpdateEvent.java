bc9b59b03c1117ffd191b941ab138750836f8552
Luãn Pereira <mrluan.gamer@outlook.com>
2017-03-02 14:35:45 -0300

    Adicionado método 'getCurrentTick' no UpdateEvent para UpdateType.TICK

--- src/br/com/battlebits/commons/bukkit/event/update/UpdateEvent.java
+++ src/br/com/battlebits/commons/bukkit/event/update/UpdateEvent.java
@@ -7,15 +7,25 @@ public class UpdateEvent extends Event {
 
 	public static final HandlerList handlers = new HandlerList();
 	private UpdateType type;
+	private long currentTick;
 
 	public UpdateEvent(UpdateType type) {
+		this(type, -1);
+	}
+	
+	public UpdateEvent(UpdateType type, long currentTick) {
 		this.type = type;
+		this.currentTick = currentTick;
 	}
 
 	public UpdateType getType() {
 		return type;
 	}
 
+	public long getCurrentTick() {
+		return currentTick;
+	}
+	
 	public HandlerList getHandlers() {
 		return handlers;
 	}
--- src/br/com/battlebits/commons/bukkit/scheduler/UpdateScheduler.java
+++ src/br/com/battlebits/commons/bukkit/scheduler/UpdateScheduler.java
@@ -7,12 +7,14 @@ import br.com.battlebits.commons.bukkit.event.update.UpdateEvent.UpdateType;
 
 public class UpdateScheduler implements Runnable {
 
+	private long tickCount = 0;
 	private long lastSecond = Long.MIN_VALUE;
 	private long lastMinute = Long.MIN_VALUE;
 
 	@Override
 	public void run() {
-		Bukkit.getPluginManager().callEvent(new UpdateEvent(UpdateType.TICK));
+		long tick = (tickCount++ % 20) + 1;
+		Bukkit.getPluginManager().callEvent(new UpdateEvent(UpdateType.TICK, tick));
 		if (lastSecond + 1000 <= System.currentTimeMillis()) {
 			Bukkit.getPluginManager().callEvent(new UpdateEvent(UpdateType.SECOND));
 			lastSecond = System.currentTimeMillis();
@@ -22,5 +24,4 @@ public class UpdateScheduler implements Runnable {
 			lastMinute = System.currentTimeMillis();
 		}
 	}
-
 }
